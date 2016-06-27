7121e81c933cc7b65da639de4d1044c24b7c7b3c
Kale Bogdanovs <kbogdanovs@smartling.com>
2016-06-27 21:35:12 +0000

    Updated '/js/toc.js' via CloudCannon

--- js/toc.js
+++ js/toc.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
 
     var ToC =
-      "<nav role='navigation' class='table-of-contents'>" +
-        "<h2>On this Page</h2>" +
         "<ul>";
     
     var newLine, el, title, link, classname;
