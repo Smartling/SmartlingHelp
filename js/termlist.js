7bb9902a1cfd900b10d98ac500382a5601873b01
Kale Bogdanovs <kbogdanovs@smartling.com>
2016-06-28 04:44:35 +0000

    Updated '/js/termlist.js' via CloudCannon

---
+++ js/termlist.js
@@ -0,0 +1,41 @@
+---
+---
+
+   terms = [
+       {% for term in terms %}
+          {{term.title}}{% unless forloop.first %},{% endunless %}
+       {% endfor %}
+       ]
+       
+   
+   
+   
+   
+   
+   
+   
+   fullterms = [
+       {
+           term: 'File',
+           definition: 'Files can be uploaded and downloaded to Smartling.',
+           links: [
+               {
+                   title: 'Working with Files',
+                   url: '/test'
+               },
+               {
+                   title: 'Not working with Files',
+                   url: '/test1'
+               }
+               ]
+       },
+       {
+           term: 'Download',
+           definition: 'Files are really easy to download',
+           links: [
+               {
+                   title: 'Get your files',
+                   url: '/test'
+               }]
+       }
+       ]
