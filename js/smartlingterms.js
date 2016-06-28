ea1e67ee6f91cd9b9c494ced642ffbd9e133112c
Kale Bogdanovs <kbogdanovs@smartling.com>
2016-06-28 04:54:21 +0000

    Updated '/js/smartlingterms.js' via CloudCannon

--- js/smartlingterms.js
+++ js/smartlingterms.js
@@ -1,32 +1,6 @@
 
 
 $(document).ready(function() {
-   terms = ["file", "download"]
-   fullterms = [
-       {
-           term: 'File',
-           definition: 'Files can be uploaded and downloaded to Smartling.',
-           links: [
-               {
-                   title: 'Working with Files',
-                   url: '/test'
-               },
-               {
-                   title: 'Not working with Files',
-                   url: '/test1'
-               }
-               ]
-       },
-       {
-           term: 'Download',
-           definition: 'Files are really easy to download',
-           links: [
-               {
-                   title: 'Get your files',
-                   url: '/test'
-               }]
-       }
-       ]
    $(".content").mark(terms, {className: 'smartling-term', exclude: ["h1", "h2", "h3", "h4", "h5", "a"]});
    var done = []
    $(".smartling-term").each(function(i) {
@@ -35,7 +9,7 @@ $(document).ready(function() {
            $(this).addClass('term-underline');
            for(i=0; i < fullterms.length; i++) {
                if(fullterms[i].term.toLowerCase() === text) {
-                   var title = fullterms[i].term
+                   var title = fullterms[i].title
                    
                    if (fullterms[i].links) {
                        var termlinks = '<ul>'
