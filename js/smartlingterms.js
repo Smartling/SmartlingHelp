2f969f26aa74d91b983bc8bea9b901f3582e2099
Kale Bogdanovs <kbogdanovs@smartling.com>
2016-06-28 01:58:41 +0000

    Updated '/js/smartlingterms.js' via CloudCannon

--- js/smartlingterms.js
+++ js/smartlingterms.js
@@ -2,6 +2,31 @@
 
 $(document).ready(function() {
    terms = ["file", "download"]
+   fullterms = [
+       {
+           term: 'File',
+           definition: 'Files can be uploaded and downloaded to Smartling.',
+           links: [
+               {
+                   title: 'Working with Files',
+                   url: '/test'
+               },
+               {
+                   title: 'Not working with Files',
+                   url: '/test1'
+               }
+               ]
+       },
+       {
+           term: 'Download',
+           definition: 'Files are really easy to download',
+           links: [
+               {
+                   title: 'Get your files',
+                   url: '/test'
+               }]
+       }
+       ]
    $(".content").mark(terms, {className: 'smartling-term', exclude: ["h1", "h2", "h3", "h4", "h5", "a"]});
    var done = []
    $(".smartling-term").each(function(index, value) {
