e16d93f71f7b944d281797c9f101a7605f1eac20
Kale Bogdanovs <kbogdanovs@smartling.com>
2016-06-28 01:06:41 +0000

    Updated '/js/smartlingterms.js' via CloudCannon

--- js/smartlingterms.js
+++ js/smartlingterms.js
@@ -1,31 +1,6 @@
 
 
-$(document).ready(function() {
-    $.expr[":"].contains = $.expr.createPseudo(function(arg) {
-        return function( elem ) {
-            return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
-        };
-    });
-    
-    
-    var terms = [
-        {
-            term: 'File',
-            definition: 'Files can be uploaded to Smartling in a bunch of ways.',
-            links: [
-                {
-                    text: 'Working with Files',
-                    link: '/articles/download-files/'
-                }
-                ]
-        }
-    ]
-    
-    for (var i=0; i < terms.length; i++) {
-        var currentTerm = terms[i];
-        $(':contains(' + currentTerm.term+ ')').html(function(_, html) {
-            var re = new RegExp('/(' + currentTerm.term + ')/','gi');
-            return  html.replace(re, '<span class="smartling-term">$1</span>')
-        });
-    }
+$(document).ready(function{
+   terms = ["file", "download"]
+   $(".content").mark(terms, {className: 'smartling-term'})
 });
