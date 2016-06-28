64c47056f446f310df9f6a9b62969d0b8680167a
Kale Bogdanovs <kbogdanovs@smartling.com>
2016-06-28 01:36:34 +0000

    Updated '/js/smartlingterms.js' via CloudCannon

--- js/smartlingterms.js
+++ js/smartlingterms.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
    $(".content").markRegExp(/download/i, {className: 'smartling-term'});
    var done = []
    $(".smartling-term").each(function(index, value) {
+       console.log(value);
        if (done.indexOf(value) === -1) {
            $(this).addClass('term-underline');
            done.push(value);
