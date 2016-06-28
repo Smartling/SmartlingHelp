a8aa22cd4a652a7da72dad7b428c7d10bfbbcf2a
Kale Bogdanovs <kbogdanovs@smartling.com>
2016-06-28 00:13:03 +0000

    Updated '/js/smartlingterms.js' via CloudCannon

---
+++ js/smartlingterms.js
@@ -0,0 +1,29 @@
+
+
+$.expr[":"].contains = $.expr.createPseudo(function(arg) {
+    return function( elem ) {
+        return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+    };
+});
+
+
+var terms = [
+    {
+        term: 'File',
+        definition: 'Files can be uploaded to Smartling in a bunch of ways.',
+        links: [
+            {
+                text: 'Working with Files',
+                link: '/articles/download-files/'
+            }
+            ]
+    }
+]
+
+for (var i=0; i < terms.length; i++) {
+    var currentTerm = terms[i];
+    $(":contains(currentTerm.term)").html(function(_, html) {
+        var re = new RegExp(currentTerm.term,"gi");
+        return  html.replace(re, '<span class="smartlingTerm">$1</span>')
+    });
+}
