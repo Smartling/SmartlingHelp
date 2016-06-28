67075c81162fd1b23dd45dd5fadad04c9f9c2e64
Kale Bogdanovs <kbogdanovs@smartling.com>
2016-06-28 21:14:50 +0000

    Updated '/js/toc.js' via CloudCannon

--- js/toc.js
+++ js/toc.js
@@ -1,33 +1,34 @@
 $(document).ready(function() {
-
-    var ToC =
-        "<ul>";
-    
-    var newLine, el, title, link, classname;
-    
-    $("h2, h3").each(function() {
-    
-      el = $(this);
-      title = el.text();
-      classname = el.prop("tagName"); 
-      el.attr("id",title.replace(/\s+/g, ''));
-      link = "#" + el.attr("id");
-    
-      newLine =
-        "<li class='toc" + classname + "'>" +
-          "<a href='" + link + "'>" +
-            title +
-          "</a>" +
-        "</li>";
-    
-      ToC += newLine;
-    
-    });
-    
-    ToC +=
-       "</ul>" +
-      "</nav>";
-    
-    $(".toc").prepend(ToC);
-
+    var l = $("h2, h3").length;
+    if (l > 1) {
+        var ToC =
+            "<ul>";
+        
+        var newLine, el, title, link, classname;
+        
+        $("h2, h3").each(function() {
+        
+          el = $(this);
+          title = el.text();
+          classname = el.prop("tagName"); 
+          el.attr("id",title.replace(/\s+/g, ''));
+          link = "#" + el.attr("id");
+        
+          newLine =
+            "<li class='toc" + classname + "'>" +
+              "<a href='" + link + "'>" +
+                title +
+              "</a>" +
+            "</li>";
+        
+          ToC += newLine;
+        
+        });
+        
+        ToC +=
+           "</ul>" +
+          "</nav>";
+        
+        $(".toc").prepend(ToC);
+    }
 });
